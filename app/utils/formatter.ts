177570b9548b4ac53f1a38f18a07b817f6246952
Jayrek <jrektabasa@gmail.com>
2025-09-17 17:04:36 +0800

    chore: add vote count formatter

---
+++ app/utils/formatter.ts
@@ -0,0 +1,11 @@
+
+export const formatVoteCount = (count?: number) => {
+    if (!count) return "0";
+    if (count >= 1_000_000) {
+      return (count / 1_000_000).toFixed(1).replace(/\.0$/, "") + "M";
+    }
+    if (count >= 1_000) {
+      return (count / 1_000).toFixed(1).replace(/\.0$/, "") + "K";
+    }
+    return count.toString();
+  };
